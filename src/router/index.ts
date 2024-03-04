13c9721baf968726164de1e73536049805ef8305
Murilo Augusto Maestro <murilomaestro@gmail.com>
2024-03-04 11:05:54 -0300

    feat: commit inicial 🎉 (boilerplate do vue.js)

---
+++ src/router/index.ts
@@ -0,0 +1,15 @@
+import DefaultView from '@/views/DefaultView.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: DefaultView
+    },
+  ]
+})
+
+export default router
