c52009da1abcfbe89621b96e387b8ded766ec210
Murilo Augusto Maestro <murilomaestro@gmail.com>
2024-03-05 15:29:53 -0300

    feat(component): icones do google

---
+++ src/components/Icons/GoogleIcon.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const { size } = defineProps({
+	size: { type: Number, default: 16 }
+})
+</script>
+<template>
+	<i class="material-symbols-outlined" :class="{ [`font-[${size}px]`]: true }" v-bind="{...$attrs}">
+		<slot></slot>
+	</i>
+</template>
+<style scoped>
+.material-symbols-outlined {
+  font-variation-settings:
+  'FILL' 1,
+  'wght' 700,
+  'GRAD' 200,
+  'opsz' 48;
+  @apply text-matisse-950
+}
+</style>
