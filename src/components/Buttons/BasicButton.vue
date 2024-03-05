6d96468bce09ba2b1b0b6c0305d589f40a891a38
Murilo Augusto Maestro <murilomaestro@gmail.com>
2024-03-05 15:29:10 -0300

    feat(components): botão basico, com modo de icone

---
+++ src/components/Buttons/BasicButton.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const { iconMode } = defineProps({
+	iconMode: { type: Boolean, default: false }
+})
+</script>
+<template>
+	<button class="button" :class="{ iconMode }">
+		<slot>aaa</slot>
+	</button>
+</template>
+<style scoped>
+.button {
+	all: unset;
+	@apply px-3 py-2 flex items-center justify-center gap-2 rounded overflow-hidden bg-matisse-50 text-matisse-850 hover:bg-matisse-200 hover:text-matisse-900
+}
+.button.iconMode {
+	@apply p-3 rounded-full
+}
+
+</style>
