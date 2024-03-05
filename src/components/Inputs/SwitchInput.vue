ca0826c28b1b5e1c72632fa82b40b62893fafe36
Murilo Augusto Maestro <murilomaestro@gmail.com>
2024-03-05 11:42:26 -0300

    feat(components): input de text e switch

---
+++ src/components/Inputs/SwitchInput.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import { SwitchRoot, SwitchThumb } from 'radix-vue';
+
+const { id } = defineProps({
+  id: { type: String, required: true },
+});
+
+const [switchState] = defineModel<boolean>({
+	required: true,
+});
+</script>
+
+<template>
+  <div class="switch-wrapper">
+	<SwitchRoot
+		v-bind:id="id"
+		v-model:checked="switchState"
+		class="switch-wrapper-root"
+	>
+		<SwitchThumb
+			class="switch-wrapper-thumb"
+		/>
+	</SwitchRoot>
+	<label class="leading-none select-none" v-bind:for="id">
+		<slot></slot>
+	</label>
+  </div>
+</template>
+
+<style>
+.switch-wrapper {
+	@apply text-matisse-800 flex gap-2 items-center transition-all
+}
+.switch-wrapper .switch-wrapper-root {
+	@apply w-[42px] h-[25px] data-[state=checked]:bg-matisse-950 bg-matisse-950/50 rounded-full outline-matisse-950 focus-within:outline focus-within:outline-matisse-950 flex relative cursor-default
+}
+.switch-wrapper .switch-wrapper-root .switch-wrapper-thumb {
+	@apply block w-[21px] h-[21px] my-auto bg-matisse-50 shadow-sm rounded-full transition-transform duration-100 translate-x-0.5 will-change-transform data-[state=checked]:translate-x-[19px]
+}
+
+
+</style>
---
+++ src/components/Inputs/TextInput.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { defineModel } from 'vue';
+
+const [valueModel] = defineModel({
+	required: true,
+});
+</script>
+<template>
+	<div class="input-wrapper">
+		<span class="input-label">
+			<slot></slot>
+		</span>
+		<input v-model="valueModel" v-bind="{...$attrs}" />
+	</div>
+</template>
+<style scoped>
+.input-wrapper {
+	@apply flex flex-col relative font-sans text-matisse-800
+}
+.input-wrapper:focus-within .input-label {
+	@apply text-matisse-950
+}
+.input-wrapper input {
+	all: unset;
+	@apply bg-matisse-100 border-solid border-2 border-transparent rounded  p-2 transition-all
+}
+.input-wrapper input:focus-within {
+	@apply bg-matisse-200 border-matisse-950
+}
+</style>
